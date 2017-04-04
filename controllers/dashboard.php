0e2d4b0326bacbc5581143c10bf343532ea7e7b9
Cristhian Pinzon <cristhianpinzon@MacBook-Air-de-Cristhian.local>
2017-04-03 20:21:09 -0500

    Se agrego footer y se mejoro el diseño con colores de acuerdo a la pp

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 } else {
    echo "Bienvenido! " . $_SESSION['username'];
    echo "Esta pagina es solo para usuarios registrados.<br>";
-   echo "<br><a href='login.html'>Login</a>";
+   echo "<br><a href='../login.html'>Login</a>";
    echo "<br><br><a href='index.html'>Registrarme</a>";
 
 exit;
