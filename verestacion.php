0b3af82f71323f025ee10bffe8a4f11e84e65082
Cristhian Pinzon <cristhianpinzon@MacBook-Air-de-Cristhian.local>
2017-04-05 22:37:16 -0500

    Cambios: 1) se realizo todo el formulario con todos los datos especificos de cada estacion 2) se mejoraron los selects de php para mostrar deptos y mayoristas 3) se creo el boton editar(sin funcionamiento aun)

--- verestacion.php
+++ verestacion.php
@@ -26,11 +26,15 @@ exit;
 
 <?php
 include_once "controllers/config.php";;
-if(isset($_POST)){
-    $idest = $_POST['idest']; //Contains the string "My value"
-    //Do something with your POST
-    
-}
+	if(isset($_POST)){
+		// id de la estacion
+	    $idest = $_POST['idest']; 
+
+		$sql = 'SELECT * FROM estacion WHERE id_estacion ='.$idest.'';  
+		$sqlDptos = 'SELECT * FROM departamento';
+		$sqlMayoristas = 'SELECT id_mayorista,marca_mayorista FROM mayorista ORDER by 1';
+		//id,nombre -> CUN,CUNDIMARCA
+	}
 
 ?>
 
@@ -64,38 +68,6 @@ if(isset($_POST)){
             </ul>
         </div>
     </nav>
-
-    <!--fin-nav-bar-->
-
-<!--     <div class="row">
-    	<div class="col s12 l12">
-    		<div class="card">
-    			<div class="card-content">
-    				
-    				<?php 
-
-    					echo '<span class="card-title">'.$idest.'</span>';
-    					// <p>I am a very simple card. I am good at containing small bits of information.
-    					// I am convenient because I require little markup to use effectively.</p>
-    				 ?>
-
-    				 
-					    <div class="card-action">
-    					<div class="right">
-	    					<button class="btn waves-effect waves-light" type="submit" name="action">Editar
-	    						<i class="material-icons right">mode_edit</i>
-	  						</button>  
-  						</div>      
-    					</div>
-
-					  </div>
-
-
-    			</div>
-    				
-    		</div>
-    	</div>
-    </div> -->
    
 <div class="container">
 
@@ -106,16 +78,233 @@ if(isset($_POST)){
 					<h4 class="header2">Datos de la estacion <?php echo $idest; ?></h4>
 					<div class="row">
 						<form class="col s12">
-							<div class="row">
-								<div class="input-field col s6">
+							
+
+							<?php 
+								//TRAER DEPARTAMENTOS
+								$deptos = array();
+
+								$resDeptos = $conn->query($sqlDptos);
+								while ($row = mysqli_fetch_assoc($resDeptos)) {
+									// lista de departamentos
+									$deptos[$row["id_departamento"]] = $row["nombre_departamento"];
+								}
+								//echo var_dump($deptos['AMA']);
+								//traermayoristas
+								$arraymayoristas = array();
+								$resMayoristas = $conn->query($sqlMayoristas);
+								while ($row = mysqli_fetch_assoc($resMayoristas)) {
+									$arraymayoristas[$row["id_mayorista"]] = $row["marca_mayorista"];
+								}
+								
+
+								// DATOS DE LA ESTACION
+								$result = $conn->query($sql);
+								$out ='';
+								if (mysqli_num_rows($result) > 0 ) {
+
+									while ($row = mysqli_fetch_array($result)) {
+										
+										$nombre = $row["nombre_estacion"];
+										$depto = $row["departamento_id_departamento"];// id departamento de la estacion
+										$lat = $row["latitud_estacion"];
+										$lon = $row["longitud_estacion"];
+										$desc = $row["descripcion_estacion"];
+
+										$tipoest = ($row["tipo_estacion"] === 'U') ? 'Urbana' : 'Rural';							
+										$tipoestArray = array('Urbana','Rural');
+
+										$telf = $row["tel_fijo_estacion"];
+										$telm = $row["tel_movil_estacion"];
+										$direc = $row["direccion_estacion"];
+										$pago = $row["pago_estacion"];
+										$estado = $row["estado_estacion"];
+										$fecha = $row["fecha_registro_estacion"];
+										$mayorista = $row["mayorista_id_mayorista"];
+										$usuario = $row["usuario_id_usuario"];
+
+
+
+										
+										
+
+										// fila 1
+										$out .= '<div class="row">';
+
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$nombre.'" type="text">
+														<label>Nombre</label>
+													</div>
+													';
+											// deptos
+											$out .= '<div class="input-field col s6">														
+														<select>
+														<option value="" disabled selected>Seleccione una opcion</option>';
+														
+											foreach ($deptos as $iddep => $nomdep) {
+												$selected = ($depto === $iddep) ? 'selected="selected"' : '';
+												$out .= '<option '.$selected.'>'.$nomdep.'</option>';
+											}
+								
+
+											$out .=	'</select>	
+													<label>Departamento</label>													
+													</div>
+
+												';
+												// fin deptos
+											$out .= '</div>';
+											
+
+										//fila 2
+										$out .= '<div class="row">';
+
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$lat.'" type="text" 
+														onkeypress="return event.charCode >= 45 && event.charCode <= 57">
+														<label>Latitud</label>
+													</div>
+													';
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$lon.'" type="text" 
+														onkeypress="return event.charCode >= 45 && event.charCode <= 57">
+														<label>Longitud</label>
+													</div>
+												';
+										$out .= '</div>';
+
+										//fila 3
+										$out .= '<div class="row">';
+
+											$out .= '<div class="input-field col s6">														
+														<select>
+														<option value="" disabled selected>Seleccione una opcion</option>';
+														
+														foreach ($tipoestArray as $numtipoest => $tipoest_arr) {
+															$selected = ($tipoest === $tipoest_arr) ? 'selected="selected"' : '';
+															$out .= '<option '.$selected.'>'.$tipoest_arr.'</option>';
+														}
+								
+
+											$out .=	'</select>	
+													<label>Tipo Estacion</label>													
+													</div>
+
+												';
+											
+											
+
+
+												//mayoristas
+											$out .= '<div class="input-field col s6">														
+														<select>
+														<option value="" disabled selected>Seleccione una opcion</option>';
+														
+											foreach ($arraymayoristas as $idmay => $marca) {
+												$selected = ($mayorista == $idmay) ? 'selected="selected"' : '';
+												$out .= '<option '.$selected.'>'.$marca.'</option>';
+											}
+								
+
+											$out .=	'</select>	
+													<label>Mayorista</label>													
+													</div>
+
+												';
+												// fin mayoristas
+											$out .= '</div>';
+
+											// fila 4
+
+											$out .= '<div class="row">';
+											$telm = ($telm === "NULL") ? "" : $telm;
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$telm.'" type="tel" >
+														<label>Telefono Movil</label>
+													</div>
+													';
+											
+
+											$telf = ($telf === "NULL") ? "" : $telf; 
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$telf.'" type="tel" >
+														<label>Telefono Fijo</label>
+													</div>
+												';
+											$out .= '</div>';
+
+											// fila 5
+											$checkedpago = ($pago == 1) ? 'checked="checked"' : '';
+
+											$out .= '<div class="row">';
+											$out .= '<div class="input-field col s3">
+														<input type="checkbox" class="filled-in" id="boxpago" '.$checkedpago.' />
+      													<label for="boxpago">Pagada</label>
+													</div>
+													';
+											$checkedestado = ($estado == 1) ? 'checked="checked"' : '';
+
+											$out .= '<div class="input-field col s3">
+														<input type="checkbox" class="filled-in" id="boxestado" '.$checkedestado.' />
+      													<label for="boxestado">Activada</label>
+													</div>
+													';
+											$out .= '<div class="input-field col s6">
+														<input disabled id="name" value="'.$fecha.'" type="text" >
+														<label>Fecha de Registro</label>
+													</div>
+												';
+											$out .= '</div>';
+
+											// fila 6
+											
+											
+											$direc = ($direc === "NULL") ? "" : $direc; 
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$direc.'" type="text" >
+														<label>Direccion</label>
+													</div>
+												';
+											
+											
+											
+											$out .= '<div class="input-field col s6">
+														<input id="name" value="'.$usuario.'" type="text" onkeypress="return event.charCode >= 47 && event.charCode <= 57">
+														<label>ID Usuario dueño </label>
+													</div>
+												';
+											$out .= '</div>';
+
+
+										// fila de textarea									
+										$desc = ($desc === "NULL") ? "" : $desc; 
+										$out .='<div class="input-field col s12">
+													<textarea id="textarea1" class="materialize-textarea">'.$desc.'</textarea>
+	          										<label>Descripcion de la estacion</label>
+												</div>';
+
+
+
+
+									}
+									echo $out;
+								}
+								// FIN DATOS ESTACION
+
+							 ?>
+							
+
+
+							<!-- 	<div class="input-field col s6">
 									<input id="name" type="text">
 									<label for="first_name">Name</label>
 								</div>
 								<div class="input-field col s6">
 									<input id="name" type="text">
 									<label for="first_name">Name2</label>
-								</div>
-							</div>
+								</div> -->
+
+							
 								<div class="row">
 									<div class="input-field col s12">
 										<button class="btn cyan waves-effect waves-light right" type="submit" name="action">
@@ -170,6 +359,8 @@ if(isset($_POST)){
         $(document).ready(function () {
 
             $(".button-collapse").sideNav();
+            $('select').material_select();
+  			$('#textarea1').trigger('autoresize');
 
         })
     </script>
